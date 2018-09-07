5b22959dbb4fc3b0f10dacf4bc9668d65b249b71
zifangsky <admin@zifangsky.cn>
2018-09-07 18:07:52 +0800

    优化代码

--- rbac_db.sql
+++ rbac_db.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50720
 File Encoding         : 65001
 
-Date: 2018-08-31 14:30:11
+Date: 2018-09-07 18:06:47
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -42,30 +42,6 @@ INSERT INTO `auth_access_token` VALUES ('1', '1.6659c9d38f5943f97db334874e522928
 INSERT INTO `auth_access_token` VALUES ('2', '1.854373728dbcc35f6f56c7671823bf5e49042f34.2592000.1537600978', '2', 'zifangsky', '2', '1537600978', 'authorization_code', 'super', '2', '2018-08-23 14:08:07', '2', '2018-08-23 15:22:59');
 
 -- ----------------------------
--- Table structure for auth_access_token_copy
--- ----------------------------
-DROP TABLE IF EXISTS `auth_access_token_copy`;
-CREATE TABLE `auth_access_token_copy` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `access_token` varchar(255) NOT NULL COMMENT 'Access Token',
-  `user_id` int(11) NOT NULL COMMENT '关联的用户ID',
-  `user_name` varchar(100) DEFAULT NULL COMMENT '用户名',
-  `client_id` int(11) NOT NULL COMMENT '接入的客户端ID',
-  `expires_in` bigint(11) NOT NULL COMMENT '过期时间戳',
-  `grant_type` varchar(50) DEFAULT NULL COMMENT '授权类型，比如：authorization_code',
-  `scope` varchar(100) DEFAULT NULL COMMENT '可被访问的用户的权限范围，比如：basic、super',
-  `create_user` int(11) DEFAULT NULL COMMENT '创建用户',
-  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
-  `update_user` int(11) DEFAULT NULL COMMENT '最后更新用户',
-  `update_time` datetime DEFAULT NULL COMMENT '最后更新时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Access Token信息表';
-
--- ----------------------------
--- Records of auth_access_token_copy
--- ----------------------------
-
--- ----------------------------
 -- Table structure for auth_client_details
 -- ----------------------------
 DROP TABLE IF EXISTS `auth_client_details`;
@@ -82,13 +58,14 @@ CREATE TABLE `auth_client_details` (
   `update_time` datetime DEFAULT NULL COMMENT '最后更新时间',
   `status` int(2) DEFAULT '0' COMMENT '0表示未开通；1表示正常使用；2表示已被禁用',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='接入的客户端信息表';
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='接入的客户端信息表';
 
 -- ----------------------------
 -- Records of auth_client_details
 -- ----------------------------
 INSERT INTO `auth_client_details` VALUES ('1', 'x3qwrgrO1wYdz72joZ8YyIuD', '测试客户端', 'DPTyJsBv1xjzEgv0MEjBnIVigaVUm3cr', 'http://127.0.0.1:7000/user/userIndex', '这是一个测试客户端服务', '1', '2018-08-06 15:17:18', '1', '2018-08-06 15:17:18', '1');
 INSERT INTO `auth_client_details` VALUES ('2', '7Ugj6XWmTDpyYp8M8njG3hqx', 'zifangsky的个人博客', 'tur2rlFfywR9OOP3fB5ZbsLTnNuNabI3', 'http://localhost:7080/login', '用于测试在客户端应用中连接授权服务器', '1', '2018-08-23 11:30:09', '1', '2018-08-23 11:30:09', '1');
+INSERT INTO `auth_client_details` VALUES ('3', 'pbAjLKuogxziSF2hGlRdl066', '测试客户端2', 'xUkqZSQQgY5SqfD9qqYXCquWRpiHTbTp', 'http://127.0.0.1:7000/user/userIndex', '这是一个测试客户端服务', '1', '2018-08-31 16:39:52', '1', '2018-08-31 16:39:52', '1');
 
 -- ----------------------------
 -- Table structure for auth_client_user
@@ -209,6 +186,7 @@ CREATE TABLE `sso_access_token` (
   `user_id` int(11) NOT NULL COMMENT '关联的用户ID',
   `user_name` varchar(100) DEFAULT NULL COMMENT '用户名',
   `ip` varchar(50) DEFAULT NULL COMMENT '用户来源IP',
+  `client_id` int(11) DEFAULT NULL,
   `channel` varchar(50) DEFAULT '' COMMENT '表示这个Token用于什么渠道，比如：官网、APP1、APP2等等',
   `expires_in` bigint(11) NOT NULL COMMENT '过期时间戳',
   `create_user` int(11) DEFAULT NULL COMMENT '创建用户',
@@ -221,10 +199,31 @@ CREATE TABLE `sso_access_token` (
 -- ----------------------------
 -- Records of sso_access_token
 -- ----------------------------
-INSERT INTO `sso_access_token` VALUES ('1', '11.0e7baee3e290429b54a5692a4eee8af5f99a9862.2592000.1538210962', '1', 'admin', '127.0.0.1', 'APP1', '1538210962', '1', '2018-08-30 16:22:00', '1', '2018-08-30 16:49:23');
-INSERT INTO `sso_access_token` VALUES ('2', '11.a138398de46e0e7963820ab24532159771251be1.2592000.1538287591', '1', 'admin', '127.0.0.1', 'TEST_CLIENT1', '1538287591', '1', '2018-08-30 17:33:26', '1', '2018-08-31 14:06:32');
-INSERT INTO `sso_access_token` VALUES ('3', '11.6ce7388a0d37c81c2a0c5661dbb94e1670a81e34.2592000.1538213738', '1', 'admin', '127.0.0.1', 'APP2', '1538213738', '1', '2018-08-30 17:35:38', '1', '2018-08-30 17:35:38');
-INSERT INTO `sso_access_token` VALUES ('4', '11.c40f4990d25fd30670d0171bc99625d3aef134d0.2592000.1538288980', '2', 'zifangsky', '127.0.0.1', 'TEST_CLIENT1', '1538288980', '2', '2018-08-31 14:11:19', '2', '2018-08-31 14:29:40');
+INSERT INTO `sso_access_token` VALUES ('1', '11.0e7baee3e290429b54a5692a4eee8af5f99a9862.2592000.1538210962', '1', 'admin', '127.0.0.1', '2', 'APP1', '1538210962', '1', '2018-08-30 16:22:00', '1', '2018-08-30 16:49:23');
+INSERT INTO `sso_access_token` VALUES ('2', '11.eb9866fa4f6dd1d4d32acb72e3db33502a7541ed.2592000.1538536378', '1', 'admin', '127.0.0.1', '1', 'TEST_CLIENT1', '1538536378', '1', '2018-08-30 17:33:26', '1', '2018-09-03 11:12:58');
+INSERT INTO `sso_access_token` VALUES ('3', '11.6ce7388a0d37c81c2a0c5661dbb94e1670a81e34.2592000.1538213738', '1', 'admin', '127.0.0.1', '3', 'APP2', '1538213738', '1', '2018-08-30 17:35:38', '1', '2018-08-30 17:35:38');
+INSERT INTO `sso_access_token` VALUES ('4', '11.c40f4990d25fd30670d0171bc99625d3aef134d0.2592000.1538288980', '2', 'zifangsky', '127.0.0.1', '1', 'TEST_CLIENT1', '1538288980', '2', '2018-08-31 14:11:19', '2', '2018-08-31 14:29:40');
+
+-- ----------------------------
+-- Table structure for sso_client_details
+-- ----------------------------
+DROP TABLE IF EXISTS `sso_client_details`;
+CREATE TABLE `sso_client_details` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `client_name` varchar(100) DEFAULT NULL COMMENT '接入单点登录的系统名称',
+  `description` varchar(1000) DEFAULT NULL,
+  `redirect_url` varchar(255) NOT NULL COMMENT '请求Token的回调URL',
+  `logout_url` varchar(255) NOT NULL COMMENT '注销URL',
+  `status` int(2) DEFAULT NULL COMMENT '0表示未开通；1表示正常使用；2表示已被禁用',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='单点登录的回调域名的白名单';
+
+-- ----------------------------
+-- Records of sso_client_details
+-- ----------------------------
+INSERT INTO `sso_client_details` VALUES ('1', 'TEST_CLIENT1', null, 'http://127.0.0.1:7000/user/userIndex', 'http://127.0.0.1:7000/logout', '1');
+INSERT INTO `sso_client_details` VALUES ('2', 'APP1', null, 'http://127.0.0.1:6080/login', 'http://127.0.0.1:6080/logout', '1');
+INSERT INTO `sso_client_details` VALUES ('3', 'APP2', null, 'http://192.168.197.130:6080/login', 'http://192.168.197.130:6080/logout', '1');
 
 -- ----------------------------
 -- Table structure for sso_refresh_token
@@ -249,23 +248,6 @@ INSERT INTO `sso_refresh_token` VALUES ('3', '1', '12.143a279cb81b0e5063af9912f3
 INSERT INTO `sso_refresh_token` VALUES ('4', '2', '12.76ea056a721c7142c3a5c48d3f1e73f627c94c2e.31536000.1567231591', '1567231591', '1', '2018-08-30 17:33:27', '1', '2018-08-31 14:06:32');
 INSERT INTO `sso_refresh_token` VALUES ('5', '3', '12.08d739a43784b1b155cf26f44bbf082f91810727.31536000.1567157738', '1567157738', '1', '2018-08-30 17:35:39', '1', '2018-08-30 17:35:39');
 INSERT INTO `sso_refresh_token` VALUES ('6', '4', '12.62f4e8fd4a41e2bf075979d3639fd9a637999548.31536000.1567232980', '1567232980', '2', '2018-08-31 14:11:19', '2', '2018-08-31 14:29:40');
-
--- ----------------------------
--- Table structure for sso_white_list
--- ----------------------------
-DROP TABLE IF EXISTS `sso_white_list`;
-CREATE TABLE `sso_white_list` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `domain` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='单点登录的回调域名的白名单';
-
--- ----------------------------
--- Records of sso_white_list
--- ----------------------------
-INSERT INTO `sso_white_list` VALUES ('1', 'http://127.0.0.1:7000/user/userIndex');
-INSERT INTO `sso_white_list` VALUES ('2', 'http://127.0.0.1:6080/login');
-INSERT INTO `sso_white_list` VALUES ('3', 'http://192.168.197.130:6080/login');
 
 -- ----------------------------
 -- Table structure for user
